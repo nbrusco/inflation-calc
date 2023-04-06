3bb843088e35cfbdfbbf32eabf2f03bc53ea82dd
Nicolas Brusco <nicolasbrusco@hotmail.com>
2023-04-06 01:33:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className='flex justify-center'>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>WELCOME TO THE UWU CALCULATOR</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          I just <code>HOPE</code> this will get us some work, paps
+        </p>
+      </div>
+      <p className="read-the-docs">
+        We could be SSRs in less than 1 year, you know.
+      </p>
+    </div>
+  )
+}
+
+export default App
